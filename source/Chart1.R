3f66c9ea926332dd4a9ed44b6d65f1f980669e73
nathanellisen <97138514+nathanellisen@users.noreply.github.com>
2022-02-17 12:43:28 -0800

    Add files via upload

---
+++ source/Chart1.R
@@ -0,0 +1,16 @@
+# Making chart for P02
+
+# histogram with bins like
+# num players with average playtime before injury being 0-5 downs, 5-10, etc
+
+library(tidyverse)
+library(ggplot2)
+
+concussions <- read.csv("https://raw.githubusercontent.com/info-201a-wi22/final-project-starter-cshoq/main/data/Concussion-Injuries-2012-2014.csv")
+View(concussions)
+
+ggplot(concussions, aes(x = Total.Snaps)) + 
+  geom_histogram(binwidth = 10, color = "darkblue", fill = "lightblue") +
+  ggtitle("Snaps Played Before Injury in Concussions from 2012-2014") + 
+  xlab("Snaps Before Injury") + ylab("# of players")
+
